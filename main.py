bc838cb1dc9453860495e250edf4dadadba0632a
knowledgekid123 <i.kasapu@outlook.com>
2024-01-06 15:38:53 -0700

    new 3:38pm

--- main.py
+++ main.py
@@ -15,19 +15,20 @@ def main():
     userInput = menu()
 
     while userInput != 0:
-        if userInput == 1:
-            display_soil_data(soil_data)
+        if userInput ==1:
+            soil_data_limit(soil_data)
         elif userInput == 2:
-            estimate_excavation_cost(soil_data)
+            display_soil_data(soil_data)
         elif userInput == 3:
+            estimate_excavation_cost(soil_data)
+        elif userInput == 4:
             plot_soil_profile(soil_data)
 
 def load_soil_data_from_csv(file_path):
     # loads soil data from a csv file 
     try:
         soil_data = pd.read_csv(file_path)
-        return soil_data.values
-        # convert DataFrame to NumPy Array
+        return soil_data.values.tolist()  # Convert DataFrame to Python list
     except FileNotFoundError:
         print("")
         print("Error: No soil data file was submitted.")
@@ -39,9 +40,10 @@ def menu():
     print("Welcome to Earth 2 Build, a premium cost and biddding estimation tool for Engineering EarthWork")
     print("")
     print("0. Exit Program")
-    print("1. Display Soil Data")
-    print("2. Estimate excavation cost")
-    print("3. Plot Soil Profile")
+    print("1. Select Elevations for Excavation")
+    print("2. Selected Soil Data")
+    print("3. Estimate Excavation Cost")
+    print("4. Plot Soil Profile")
     print()
     
     userInput = int(input("userInput (0-3)? "))
@@ -49,12 +51,34 @@ def menu():
         userInput = int(input("userInput (0-3)? "))
     return userInput
 
+def soil_data_limit(soil_data):
+    min_elevation, max_elevation = soil_data_min_max(soil_data)
+    print("Min Elevation:", min_elevation)
+    print("Max Elevation:", max_elevation)
+    limit_value = float(input("Limit Value? "))
+    
+    while not (min_elevation <= limit_value <= max_elevation):
+        limit_value = float(input("Limit Value? "))
+    
+    p = 1
+    while p < len(soil_data):
+        if soil_data[p][2] > limit_value:  # Assuming the third column represents elevation
+            del soil_data[p]
+        else:
+            p += 1
+
+def soil_data_min_max(soil_data):
+    elevations = [entry[2] for entry in soil_data]  # Assuming the third column represents elevation
+    min_elevation = min(elevations)
+    max_elevation = max(elevations)
+    return min_elevation, max_elevation    
+
 def display_soil_data(soil_data):
     print("")
     print("SOIL DATA:")
     print("%-9.9s  %-9.9s  %-15.15s" % ("Northing", "Easting", "Elevation(m)"))
     
-    for k in range(1, len(soil_data)):
+    for k in range(0, len(soil_data)):
         northing = soil_data[k][0]
         easting = soil_data[k][1]
         elevation = soil_data[k][2]
