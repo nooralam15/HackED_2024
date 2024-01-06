0062262eda0fc162263d944a3ce04b5cb631bb4a
knowledgekid123 <i.kasapu@outlook.com>
2024-01-06 13:50:04 -0700

    more script

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 
+def main():
+    #load soil data from CSV file
+    soil_data = load_soil_data_from_csv("soil_data.csv")
+
+    #display information about the soil data
+    #display_soil_info()
+
+    userInput = menu()
+    while userInput != 0:
+        if userInput == 1:
+            display_soil_info(soil_data)
+        elif userInput == 2:
+            estimate_excavation_cost(soil_data)
+        elif userInput == 3:
+            plot_soil_profile(soil_data)
+
 def load_soil_data_from_csv(file_path):
     # loads soil data from a csv file 
     try:
@@ -20,25 +36,24 @@ def load_soil_data_from_csv(file_path):
 #This function will create a menu that will loop through the code
 def menu():
     print("Welcome to Earth 2 Build, a premium cost and biddding estimation tool for Engineering EarthWork")
-    userInput = input("Please Pick a Number from the Menu: \n 1. Upload Elevation Data \n 2. Estimate Excavation Cost \n 3. Plot Soil Profile \n 0. Quit Program ")
-
+    print("")
+    print("0. Exit Program")
+    print("1. Display Soil Data")
+    print("2. Estimate excavation cost")
+    print("3. Plot Soil Profile")
+    print()
+    
+    userInput = int(input("userInput (0-3)?"))
+    while not (0 <= userInput <=3):
+        userInput = int(input("userInput (0-3)?"))
     return userInput
 
-def main():
-    #load soil data from CSV file
-    soil_data = load_soil_data_from_csv("soil_data.csv")
-
-    #display information about the soil data
-    #display_soil_info()
-
-    choice = menu()
-    while choice != 0:
-        if choice == 1:
-            display_soil_info(soil_data)
-        elif choice == 2:
-            estimate_excavation_cost(soil_data)
-        elif choice == 3:
-            plot_soil_profile(soil_data)
+def display_soil_data(soil_data):
+    print("")
+    print("SOIL DATA: ")
+    print("Northing Easting Elevation(m)")
+    for row  in soil_data:
+        print("%-3d %=14s")
 
 main()
 
