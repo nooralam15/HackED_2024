dbf88a6a8d3904231a2d0c8e6f6dbeca1c91335e
knowledgekid123 <i.kasapu@outlook.com>
2024-01-06 13:07:21 -0700

    main

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ def main():
     display_soil_info()
 
     choice = menu()
+
+    while choice != 0:
+        if choice == 1:
+            display_soil_info(soil_data)
+        elif choice == 2:
+            estimate_excavation_cost(soil_data)
+        elif choice == 3:
+            plot_soil_profile(soil_data)
+
+        choice = menu()
