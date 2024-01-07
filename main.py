b4903104019119a703d57de6bc67e72622e30819
knowledgekid123 <i.kasapu@outlook.com>
2024-01-06 18:12:02 -0700

    18:11

--- main.py
+++ main.py
@@ -41,13 +41,13 @@ def load_soil_data_from_csv(file_path):
 #This function will create a menu that will loop through the code
 def menu():
     print("")    
-    print("Welcome to Earth 2 Build, a premium cost and biddding estimation tool for Engineering EarthWork")
+    print("Welcome to Earth-2-Build, a premium cost and bidding estimation tool for Civil Engineering Earthwork")
     print("")
     print("0. Exit Program")
-    print("1. Base Elevation Limit Value")
+    print("1. Maximum Elevation Limit")
     print("2. Updated Soil Data")
     print("3. Estimate Excavation Cost")
-    print("4. Plot Soil Profile")
+    print("4. Plot Updated Soil Profile")
     print()
     
     userInput = int(input("userInput (0-4)? "))
@@ -57,19 +57,19 @@ def menu():
 
 def soil_data_limit(soil_data):
     min_elevation, max_elevation = soil_data_min_max(soil_data)
-    print("Min Elevation:", min_elevation)
-    print("Max Elevation:", max_elevation)
+    print("Minimum Elevation:", min_elevation)
+    print("Maximum Elevation:", max_elevation)
 
     # Handle input for the limit value with proper error checking
     valid=True
     while valid:
-        limit_value_str = input("Base Elevation Limit Value? ")
+        limit_value_str = input("Maximum Elevation Limit? ")
         try:
             limit_value = float(limit_value_str)
             if min_elevation <= limit_value and limit_value <= max_elevation:
                 valid = False  # Exit the loop if a valid float is entered within the range
             else:
-                print("Invalid input. Base Elevation must be within the range.")
+                print("Invalid input. Maximum Elevation must be within the range.")
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
@@ -80,7 +80,7 @@ def soil_data_limit(soil_data):
         else:
             p = p + 1
 
-    print("Soil data filtered based on Base Elevation Limit.")
+    print("Soil data filtered based on Maximum Elevation Limit.")
     print("")
     return soil_data
 
@@ -103,6 +103,10 @@ def display_soil_data(soil_data):
         
         print("%-9.1f  %-9.1f  %-15.1f" % (northing, easting, elevation))
 
+# calculates the cut and fill needed for earthwork
+def estimate_excavation_cost(soil_data):
+
+
 # This function will plot the csv soil data as a contour map
 def plot_soil_profile(data):
     # Seperates data points into Northing, Easting, and Elevations
@@ -115,7 +119,7 @@ def plot_soil_profile(data):
     # Creating a contour plot
     plt.figure(figsize=(8, 6))
     contour_plot = plt.tricontourf(northing, easting, elevation, levels=20, cmap='viridis')
-    plt.colorbar(contour_plot, label='Elevation')  # Add a colorbar for reference
+    plt.colorbar(contour_plot, label='Elevation')  # Add a colourbar for reference
     plt.xlabel('Northing')
     plt.ylabel('Easting')
     plt.title('2D Contour Map')
